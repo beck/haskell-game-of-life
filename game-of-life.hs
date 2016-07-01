1c6180ecd85871823764459f4dad2e3fabaa4b9a
Doug Beck <doug@douglasbeck.com>
2016-07-01 01:21:12 -0400

    Probably terrible but lgtm

--- game-of-life.hs
+++ game-of-life.hs
@@ -1,8 +1,22 @@
 module Main where
 import Control.Concurrent
+import System.Console.ANSI
 
-main :: IO ()
+
+duplicate :: String -> Int -> String
+duplicate string n = concat $ replicate n string
+
+
+line 0 = return ()
+
+line n = do
+  line (n - 1)
+  clearScreen
+  threadDelay 100000
+  putStrLn (duplicate "-" n)
+
+
 main = do
-  putStrLn "Hello, Haskell!"
-  threadDelay 3000000
-  putStrLn "Goodbye"
+  line 10
+  clearScreen
+  putStrLn "done"
