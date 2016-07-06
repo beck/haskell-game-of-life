71c7f6332b1b7dc3516f261fe68a225314b426a0
Doug Beck <doug@douglasbeck.com>
2016-07-06 00:05:43 -0400

    Have a stateful box that updates randomly

--- game-of-life.hs
+++ game-of-life.hs
@@ -1,15 +1,27 @@
 module Main where
 import Control.Concurrent
 import System.Console.ANSI
+import System.Random
 import Data.ByteString.UTF8 (fromString)
+
+width = 20
 
 dot True = "●"
 dot False = " "
 unrow a = unwords (map dot a)
 untable a = unlines (map unrow a)
-display a = putStr (untable a)
+
+mkrow i a = take width (drop (i * width) a)
+mktable a = [mkrow i a | i<-[1..(width-1)]]
+display s = untable (mktable s)
 
 main = do
   clearScreen
-  let a = [[True, True, True], [True, True, True], [True, True, True]]
-  display a
+  g <- newStdGen
+  putStr "Seed: "
+  putStrLn (head (words (show g)))
+  let s = (take (width * width) (randoms g)) :: [Bool]
+  putStr (display s)
+  let sec = 1000000
+  threadDelay (round (sec/4))
+  main
