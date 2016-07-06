c2566ce02b6a3cbeb7eb030f34802e6ef6b8f91e
Doug Beck <doug@douglasbeck.com>
2016-07-05 23:03:45 -0400

    Much cleaner (sorry polymorph)

--- game-of-life.hs
+++ game-of-life.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE FlexibleInstances #-}
-
 module Main where
 import Control.Concurrent
 import System.Console.ANSI
@@ -7,20 +5,11 @@ import Data.ByteString.UTF8 (fromString)
 
 dot True = "●"
 dot False = " "
-
-class Display a where
-  display :: a -> String
-
-instance Display Bool where
-  display a = dot a
-
-instance Display [Bool] where
-  display a = unwords (map dot a)
-
-instance Display [[Bool]] where
-  display a = unlines (map display a)
+unrow a = unwords (map dot a)
+untable a = unlines (map unrow a)
+display a = putStr (untable a)
 
 main = do
   clearScreen
   let a = [[True, True, True], [True, True, True], [True, True, True]]
-  putStr (display a)
+  display a
